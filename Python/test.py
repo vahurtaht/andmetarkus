c0f59e628759c91ff507e3467915899942e81616
vahurt <vahur.taht@gail.com>
2025-09-08 09:09:52 +0300

    Message

--- Python/test.py
+++ Python/test.py
@@ -1,10 +1,24 @@
-from turtle import *
+# NAME EXERCISE
+first_name = "Kaido"
+surname = "Vetevoog"
+full_name = first_name + " " + surname
+print(full_name)
+full_name_search = full_name.upper()
+print(full_name_search)
 
-forward(100)
-left(120)
-forward(100)
-left(120)
-forward(100)
-left(120)
 
-exitonclick() 
+# NUMBERS EXERCISE  
+age = 150
+height = 1.75
+print(f"Mu vanus on {age} ja pikkus on {height}.")
+
+# Mida väljastab järgmine koodirida?
+print(age * first_name)
+
+# Hashed name
+hashed_name = surname[0] + (len(surname)-1)*surname[0].lower()
+print(hashed_name)
+
+# Initials
+initials = first_name[:2] + surname[:2]
+print(initials)
